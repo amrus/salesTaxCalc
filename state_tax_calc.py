8dceeaf4c0d7d83be5018168ddcd29363e350990
alana <alanarust@gmail.com>
2020-12-01 19:48:45 -0800

    Add files via upload

---
+++ state_tax_calc.py
@@ -0,0 +1,11 @@
+# state_tax_calculator.py
+# author: amrus (Alana)
+# date: 05-23-2020
+# tax rate based on 06-08-2020
+
+totalCost = float(input('Enter the total cost of the item: '))
+totalTax = float(input('Enter the state or country tax: '))
+
+calcCost = totalCost + totalTax
+print(calcCost)
+print(state)
